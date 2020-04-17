eacbeaad0e56c7d782b31667f647c7b3aa669bb4
AriasJaime <arias.jaime98@gmail.com>
2020-04-17 18:41:54 +0200

    CSS integrado a profile change

--- src/components/Clase.js
+++ src/components/Clase.js
@@ -10,7 +10,7 @@ export default class SubjectList extends React.Component {
         
         <ul class="containerSub">
 
-              <li class= "flex-obj"> profesor:  {this.props.profe } </li> 
+              <li class= "flex-obj"> profesor :  {this.props.profe } </li> 
               <li class= "flex-obj"> {this.props.asignatura } </li>
               <li class= "flex-obj"> {this.props.precio} kc</li>
          
